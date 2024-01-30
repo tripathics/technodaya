2769f782eb78185b4f582ff6784179e3d08a3700
Keshav <keshavbhutaani1360@gmail.com>
2024-01-30 19:36:31 +0530

    Added Visitor Count in Footer

--- app/(pages)/layout.js
+++ app/(pages)/layout.js
@@ -1,13 +1,22 @@
+'use client'
 import Navbar from '@/components/navbar/';
 import Footer from '@/components/footer/';
 import styles from './layout.module.scss';
+import { useEffect,useState } from 'react';
 
 export default function Layout({ children }) {
+  const [Visitors, setVisitors] = useState(0)
+  useEffect(()=>{
+    const storedCount = localStorage.getItem("page-Visits");
+    const initialCount = Number(storedCount) || 1052;
+    setVisitors(initialCount+1);
+    localStorage.setItem("page-Visits",initialCount+1);
+  },[]);
   return (<>
     <Navbar />
     <main className={styles.main}>
       {children}
     </main>
-    <Footer />
+    <Footer Visits={Visitors} />
   </>)
 }
